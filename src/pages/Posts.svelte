4d262c0d8c14f2a587d95c29206aa2c524f98ba6
Urban <josh@misterslandscaping.com>
2023-09-26 17:28:09 -0400

    Fix

--- src/pages/Posts.svelte
+++ src/pages/Posts.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
-    import {Gitlab} from '@gitbeaker/rest'
+    import {Gitlab, type DiscussionSchema, type ProjectSchema, type DiscussionNoteSchema} from '@gitbeaker/rest'
+	import { writable } from 'svelte/store';
     const api = new Gitlab({
         token: 'glpat-vvjrhxHyR9-deHhGTUfd'
     });
-    api.dis
+    const notes = writable<DiscussionNoteSchema[]>([]);
+    //api.Projects.show('50732431').then((p: ProjectSchema) => console.log());
+    
+    api.IssueDiscussions.all('50732431', 1).then((discussions) => {
+        discussions.forEach(discussion => {
+            if(discussion.individual_note && discussion.notes) {
+                notes.update((prev) => {
+                    if(discussion.notes){
+                        return [...prev, ...discussion.notes ]
+                    }
+                    return [...prev];
+                    
+                })
+            }
+        });
+    });
+    $: console.log($notes);
+
+    
+
 </script>
+<ul>
+    {#each $notes as note}
+    <li>{note.body}</li>
+{/each}
+</ul>
