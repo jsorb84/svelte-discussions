507cba28b6f0fef42877d006eaec29ee485d427f
jsorb84 <105475254+jsorb84@users.noreply.github.com>
2023-09-26 22:30:50 -0400

    Init

--- src/pages/Posts.svelte
+++ src/pages/Posts.svelte
@@ -1,32 +1,35 @@
 <script lang="ts">
-    import {Gitlab, type DiscussionSchema, type ProjectSchema, type DiscussionNoteSchema} from '@gitbeaker/rest'
-	import { writable } from 'svelte/store';
-    const api = new Gitlab({
-        token: 'glpat-vvjrhxHyR9-deHhGTUfd'
-    });
-    const notes = writable<DiscussionNoteSchema[]>([]);
-    //api.Projects.show('50732431').then((p: ProjectSchema) => console.log());
-    
-    api.IssueDiscussions.all('50732431', 1).then((discussions) => {
-        discussions.forEach(discussion => {
-            if(discussion.individual_note && discussion.notes) {
-                notes.update((prev) => {
-                    if(discussion.notes){
-                        return [...prev, ...discussion.notes ]
-                    }
-                    return [...prev];
-                    
-                })
-            }
-        });
-    });
-    $: console.log($notes);
-
-    
-
+	import { Octokit } from "@octokit/rest";
+	import { writable } from "svelte/store";
+	import type {
+		GetResponseDataTypeFromEndpointMethod,
+		OctokitResponse,
+	} from "@octokit/types";
+	import Markdown from "@magidoc/plugin-svelte-marked";
+	type GithubIssue = GetResponseDataTypeFromEndpointMethod<
+		typeof kit.issues.listForRepo
+	>;
+	type Posts = GithubIssue;
+	const kit = new Octokit({
+		auth: "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",
+	});
+	const postsStore = writable<Posts | null>(null);
+	kit.rest.issues
+		.listForRepo({
+			owner: "jsorb84",
+			repo: "svelte-discussions-blog",
+		})
+		.then((posts: OctokitResponse<GithubIssue>) => {
+			let data: Posts = posts.data;
+			postsStore.set(data);
+		});
 </script>
-<ul>
-    {#each $notes as note}
-    <li>{note.body}</li>
-{/each}
-</ul>
+
+{#if $postsStore}
+	{#each $postsStore as post}
+		<hr />
+		<span>{post.title}</span>
+		<Markdown source="{post.body}" />
+		<hr />
+	{/each}
+{/if}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import vitePluginSingleSpa from "vite-plugin-single-spa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+	plugins: [
+		svelte(),
+		vitePluginSingleSpa({
+			serverPort: 4101,
+			spaEntryPoint: "/index.html",
+		}),
+	],
+});
